45db5d20e2760093871b308dbf541a58fe90d9bf
akurae <a.a.kurilovich@yandex.ru>
2017-10-12 12:01:48 +0300

    json_load and json_pretty_print functions are updated

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,15 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r', encoding='utf-8') as fin:
+        return json.load(fin)
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input()
+    data = load_data(filepath)
+    pretty_print_json(data)
